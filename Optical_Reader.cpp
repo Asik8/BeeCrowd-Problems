c152d0b31a732a066e05db45b697ad8bfb93f69e
Asik8 <motasembillah84@gmail.com>
2024-06-19 11:38:57 +0600

    another done

--- Optical_Reader.cpp
+++ Optical_Reader.cpp
@@ -1,1 +1,35 @@
-git 
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    while (1)
+    {
+        cin >> t;
+        if (t > 0)
+        {
+            while (t--)
+            {
+                ll ar[5];
+                for (int i = 0; i < 5; i++) cin >> ar[i];
+                int c = 0;
+                for (int i = 0; i < 5; i++) if (ar[i] <= 127) c++;
+                if (c == 1)
+                {
+                    for (int i = 0; i < 5; i++) if (ar[i] <= 127) c = i;
+                    if (c == 0) cout << "A\n";
+                    if (c == 1) cout << "B\n";
+                    if (c == 2) cout << "C\n";
+                    if (c == 3) cout << "D\n";
+                    if (c == 4) cout << "E\n";
+                }
+                else cout << "*\n";
+            }
+        }
+        else return 0;
+    }
+}
